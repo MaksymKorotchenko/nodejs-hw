f6121e53dbea2520a5c2955753d068dac6268133
MaksymKorotchenko <maximuskor29@gmail.com>
2025-09-27 21:41:15 +0300

    upd

--- src/server.js
+++ src/server.js
@@ -25,14 +25,14 @@ app.use(
   }),
 );
 
-app.get('/notes', (reg, res) => {
+app.get('/notes', (req, res) => {
   res.status(200).json({
     message: 'Retrieved all notes',
   });
 });
 
-app.get('/notes/:noteId', (reg, res) => {
-  const { noteId } = reg.params;
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
   res.status(200).json({ message: `Retrieved note with ID:${noteId}` });
 });
 
@@ -48,7 +48,7 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   res.status(500).json({
-    message: 'Simulated server error',
+    message: err.message,
   });
   next();
 });
